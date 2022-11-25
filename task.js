2548be9bba4cc9cb32b9f33b6fad40e08bdc26a1
Divyatestgithub <118978781+Divyatestgithub@users.noreply.github.com>
2022-11-25 11:28:00 +0530

    Update task.js

--- task.js
+++ task.js
@@ -5,14 +5,15 @@ let str = `{"input":
                      }
                     }`;
     
-        let actual_object = JSON.parse(str); 
-        console.log(actual_object);
-        let actual_object_return = actual_object.input;
-        console.log(actual_object_return);
-        let new_obj = {};
-        let final_obj = {};
-        Object.keys(actual_object_return).forEach(function(key) {
-            new_obj[key] = actual_object_return[key]["name"];
-        });
-        final_obj['input']=new_obj;
-        console.log(final_obj.input.output1);
+let actual_object = JSON.parse(str); 
+console.log(actual_object);
+alert(JSON.stringify(actual_object));
+let actual_object_return = actual_object.input;
+console.log(actual_object_return);
+let new_obj = {};
+let final_obj = {};
+Object.keys(actual_object_return).forEach(function(key) {
+    new_obj[key] = actual_object_return[key]["name"];
+});
+final_obj['input']=new_obj;
+console.log(final_obj.input.output1);
